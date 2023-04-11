6cb8fc99911c2efe6d5ed9de6132c826847e978e
moyhig-ecs <u416528d@ecs.osaka-u.ac.jp>
2023-04-11 14:54:15 +0900

    .

---
+++ _build/jupyter_execute/chapters/01/intro.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 学問への扉
+# ## プログラミングで遡る科学史
+# 
+# ### 授業の説明
+# 
+# * 初回
+#   * JupyterHub
+#     - github pagesへのアクセスとJupyterHubへの移動
+#     - OUMailによる多要素認証
+#     - 簡単なセルの実行
+#   * ChatGPT (OpenAI API) キーの取得
+#     - アカウントの作成
+#     - (本授業の組織への参加)
+#   * JupyterHub + ChatGPI
+#     - アプリケーションキーの登録
+#     - チャットによるプログラミング
+# 
+# 
+# 
+
+# In[1]:
+
+
+1+1
+
